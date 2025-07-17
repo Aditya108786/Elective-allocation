d5041484f3d8faf73c3ed2294ac58c0cfa135904
aditya singh <aditya.23cse362@rtu.ac.in>
2025-07-17 20:39:30 +0530

    Removed deprecated MongoDB options: useNewUrlParser and useUnifiedTopology

--- elective-allocation/config/db.js
+++ elective-allocation/config/db.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () =>{
     try {
-        await mongoose.connect(process.env.MONGODB_URI , {
-            UseNewUrlParser:true,
-            useUnifiedTopology:true
-        });
+        await mongoose.connect(process.env.MONGODB_URI );
         console.log('mongodb connected')
     } catch (error) {
       console.error('connection failed', error.message) 
